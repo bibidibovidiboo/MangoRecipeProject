025150a6813f58c4775e2cae51c0558343816bfe
seungwonleee <seungwon.code@gmail.com>
2020-11-26 23:50:51 +0900

    냉장고 드래그앤드롭 추가

--- client/src/components/sections/main/Main.js
+++ client/src/components/sections/main/Main.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Slider from '../../slider/Slider';
+import Refrigerator from '../../refrigerator/Refrigerator';
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 function Main() {
     return (
         <div>
             <Slider />
-            여기는 Main 페이지
+            여기는 Main 메인 페이지 입니다
+
+            {/* 냉장고 드래그엔 드롭 */}
+            <DndProvider backend={HTML5Backend}>
+                <Refrigerator />
+            </DndProvider>
+
         </div>
     )
 }
